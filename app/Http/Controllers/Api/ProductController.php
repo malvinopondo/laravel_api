ec7de6ad9eb6c0a0e50554a8f30c2517ddcc09ce
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-09-06 01:11:46 +0300

    Added a Resources folder in app

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -1,13 +1,36 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
+use App\Models\Product;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index(){
+        $products = Product::get();
+        if($products){
+
+        }
+        else{
+            return response ()->json(['message' => "No record available"], 200);
+        }
+    }
+
+    public function store(){
         
     }
+
+    public function show(){
+        
+    }
+
+    public function update(){
+        
+    }
+
+    public function destroy(){
+        
+    }
+
 }
